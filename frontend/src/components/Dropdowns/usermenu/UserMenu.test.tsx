52db2811f03d8ed59357357ffdb51064402a169a
Saad Salman <saad.salman@emumba.com>
2023-03-27 13:26:57 +0500

    UserNavMenu component and tests updated

--- frontend/src/components/Dropdowns/usermenu/UserMenu.test.tsx
+++ frontend/src/components/Dropdowns/usermenu/UserMenu.test.tsx
@@ -6,11 +6,33 @@ import { AUTH_STATE_MOCK } from '../../../__mocks__/auth';
 import NavUserMenu from './UserMenu';
 
 describe('NavUserMenu component', () => {
-  test('should render avatar and menu options', () => {
-    const { getByTestId, getByText } = renderWithProviders(<NavUserMenu />);
+  test('render avatar and should not render menu options', () => {
+    const { getByTestId, queryByText } = renderWithProviders(<NavUserMenu />);
 
     const avatarElement = getByTestId('nav-avatar');
     expect(avatarElement).toBeInTheDocument();
+
+    const createGistLink = queryByText('Create Gist');
+    expect(createGistLink).not.toBeInTheDocument();
+
+    const myGistsLink = queryByText('My Gists');
+    expect(myGistsLink).not.toBeInTheDocument();
+
+    const starredGistsLink = queryByText('Starred Gists');
+    expect(starredGistsLink).not.toBeInTheDocument();
+
+    const profileLink = queryByText('Profile');
+    expect(profileLink).not.toBeInTheDocument();
+
+    const logoutLink = queryByText('Logout');
+    expect(logoutLink).not.toBeInTheDocument();
+  });
+
+  test('should render menu options when avatar is clicked', () => {
+    const { getByTestId, getByText } = renderWithProviders(<NavUserMenu />);
+
+    const avatarElement = getByTestId('nav-avatar');
+    fireEvent.click(avatarElement);
 
     const createGistLink = getByText('Create Gist');
     expect(createGistLink).toBeInTheDocument();
@@ -31,7 +53,11 @@ describe('NavUserMenu component', () => {
   test('should handle logout', () => {
     const authState = store.getState().auth;
     jest.spyOn(Storage.prototype, 'clear');
-    const { getByText } = renderWithProviders(<NavUserMenu />);
+
+    const { getByText, getByTestId } = renderWithProviders(<NavUserMenu />);
+    const avatarElement = getByTestId('nav-avatar');
+    fireEvent.click(avatarElement);
+
     const logoutLink = getByText('Logout');
     fireEvent.click(logoutLink);
     expect(localStorage.clear).toHaveBeenCalled();
@@ -41,22 +67,35 @@ describe('NavUserMenu component', () => {
   });
 
   test('should navigate to correct link when clicked', () => {
-    const { getByText } = renderWithProviders(<NavUserMenu />);
+    const { getByText, getByTestId } = renderWithProviders(<NavUserMenu />);
+    const avatarElement = getByTestId('nav-avatar');
 
+    fireEvent.click(avatarElement);
     const createGistLink = getByText('Create Gist');
     fireEvent.click(createGistLink);
     expect(window.location.pathname).toBe('/create-gist');
 
+    fireEvent.click(avatarElement);
     const myGistsLink = getByText('My Gists');
     fireEvent.click(myGistsLink);
     expect(window.location.pathname).toBe('/my-gists');
 
+    fireEvent.click(avatarElement);
     const starredGistsLink = getByText('Starred Gists');
     fireEvent.click(starredGistsLink);
     expect(window.location.pathname).toBe('/starred-gists');
 
+    fireEvent.click(avatarElement);
     const profileLink = getByText('Profile');
     fireEvent.click(profileLink);
     expect(window.location.pathname).toBe('/profile');
+  });
+
+  test('should close the menu when clicking outside', () => {
+    const { getByTestId, queryByTestId } = renderWithProviders(<NavUserMenu />);
+    fireEvent.click(getByTestId('nav-avatar'));
+    expect(getByTestId('user-menu-body')).toBeInTheDocument();
+    fireEvent.click(document.body);
+    expect(queryByTestId('user-menu-body')).not.toBeInTheDocument();
   });
 });
--- frontend/src/components/Dropdowns/usermenu/UserMenu.tsx
+++ frontend/src/components/Dropdowns/usermenu/UserMenu.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../../store';
@@ -9,50 +10,73 @@ function NavUserMenu() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const avatarUrl =  useSelector(getUserAvatar);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const handleLogout = () => {
     dispatch(resetAuthData());
     window.localStorage.clear();
     navigate('/');
+    setMenuOpen(false);
   }
 
-  const handleClick = () => {
-    const element = document.activeElement;
-    if (element instanceof HTMLElement){
-      element.blur();
+  const handleClick = (e: React.MouseEvent) => {
+    setMenuOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+  const handleClickOutside = (e: any) => {
+    if (menuRef && menuRef.current && !menuRef.current.contains(e.target)) {
+      setMenuOpen(false);
     }
   };
 
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div data-testid="nav-avatar" className="dropdown dropdown-end">
-      <label tabIndex={0} className='cursor-pointer'>
+    <div data-testid="user-menu" className="dropdown dropdown-end" ref={menuRef}>
+      <label tabIndex={0} data-testid="nav-avatar" className="cursor-pointer" onClick={toggleMenu} onKeyUp={toggleMenu}>
         <Avatar url={avatarUrl} />
       </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <Link to='/create-gist' className="justify-between" onClick={handleClick}>
-            Create Gist
-            {/* <span className="badge">New</span> */}
-          </Link>
-        </li>
-        <li>
-          <Link to='/my-gists' className="justify-between" onClick={handleClick}>
-            My Gists
-            {/* <span className="badge">New</span> */}
-          </Link>
-        </li>
-        <li>
-          <Link to='/starred-gists' className="justify-between" onClick={handleClick}>
-            Starred Gists
-          </Link>
-        </li>
-        <li>
-          <Link to='/profile' className="justify-between" onClick={handleClick}>
-            Profile
-          </Link>
-        </li>
-        <li onClick={handleLogout}><a>Logout</a></li>
-      </ul>
+      {menuOpen && (
+        <ul tabIndex={0} data-testid="user-menu-body" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <Link to='/create-gist' className="justify-between" onClick={handleClick}>
+              Create Gist
+              {/* <span className="badge">New</span> */}
+            </Link>
+          </li>
+          <li>
+            <Link to='/my-gists' className="justify-between" onClick={handleClick}>
+              My Gists
+              {/* <span className="badge">New</span> */}
+            </Link>
+          </li>
+          <li>
+            <Link to='/starred-gists' className="justify-between" onClick={handleClick}>
+              Starred Gists
+            </Link>
+          </li>
+          <li>
+            <Link to='/profile' className="justify-between" onClick={handleClick}>
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link to='/' className="justify-between" onClick={handleLogout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
